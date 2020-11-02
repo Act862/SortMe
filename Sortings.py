bdfe9e10e72238784b545ec863eb29af799d33f2
TheGodOfAct <38786903+Act862@users.noreply.github.com>
2020-11-02 14:40:08 +0200

    Create Sortings.py
    first step, initial class

---
+++ Sortings.py
@@ -0,0 +1,12 @@
+# this is a module containing sorting algorithms logic
+class SortEl:
+    def __init__(self):
+        self.data = []
+        self.length = 0
+        self.sorted = False
+    
+    def isSorted(self):
+        return self.sorted
+
+    def sort(self,algo):
+        
